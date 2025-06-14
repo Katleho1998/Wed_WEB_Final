039b7821b762f885cb8529e40454eda8bf6c3139
Katleho1998 <142925628+Katleho1998@users.noreply.github.com>
2025-06-14 04:43:06 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -30,8 +30,7 @@ function App() {
         <main>
           <HeroSection />
           {/* Modern Countdown Timer - Moved up more */}
-          <div className="-mt-18 mb-20
-            w-full flex flex-col items-center">
+          <div className="-mt-4 mb-6 w-full flex flex-col items-center">
             <CountdownTimer />
           </div>
           <OurStory />
